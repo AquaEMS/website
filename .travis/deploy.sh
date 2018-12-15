7a843037116bc21b376e81018cff0e9ddd89f3e6
Dan Bruce <dan@ddbruce.com>
2018-12-14 19:23:20 -0600

    modify deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,3 +3,5 @@ chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
 ssh -o StrictHostKeyChecking=no travis@aquaems.com "/home/travis/deploy_aquaems.sh"
+
+exit 1
