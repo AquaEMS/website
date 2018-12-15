7d135c4599192800d9a87a0b75ac6307bc47b3b8
Dan Bruce <dan@ddbruce.com>
2018-12-14 19:08:59 -0600

    StrictHostKeyChecking=no

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,4 +2,4 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
-ssh travis@aquaems.com "/home/travis/deploy_aquaems.sh"
+ssh StrictHostKeyChecking=no travis@aquaems.com "/home/travis/deploy_aquaems.sh"
